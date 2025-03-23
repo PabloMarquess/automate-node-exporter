de6cfcd6f726088012ccae29d0c7390254221ce4
Pablo Marques <marques.aln@gmail.com>
2025-03-22 21:23:21 -0300

    refactor: atualizando template

--- install.sh
+++ install.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# Detect the architecture
+# Verifica arquitetura;
 ARCH=$(uname -m)
 
-# Determine the appropriate download link
+# Determina o Link apropriado;
 if [ "$ARCH" == "x86_64" ]; then
-    NODE_EXPORTER_URL="https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz"
-    NODE_EXPORTER_DIR="node_exporter-1.8.1.linux-amd64"
+    NODE_EXPORTER_URL="https://github.com/prometheus/node_exporter/releases/download/v1.9.0/node_exporter-1.9.0.linux-amd64.tar.gz"
+    NODE_EXPORTER_DIR="node_exporter-1.9.0.linux-amd64"
 elif [ "$ARCH" == "aarch64" ]; then
-    NODE_EXPORTER_URL="https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-arm64.tar.gz"
-    NODE_EXPORTER_DIR="node_exporter-1.8.1.linux-arm64"
+    NODE_EXPORTER_URL="https://github.com/prometheus/node_exporter/releases/download/v1.9.0/node_exporter-1.9.0.linux-arm64.tar.gz"
+    NODE_EXPORTER_DIR="node_exporter-1.9.0.linux-arm64"
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
 
-# Download and extract node exporter
+# Baixa e extrai o node-exporter
 cd /tmp
 wget $NODE_EXPORTER_URL -O node_exporter.tar.gz
 tar -xvf node_exporter.tar.gz
 cd $NODE_EXPORTER_DIR
 
-# Move node_exporter binary to /usr/local/bin
+# Move o binario do node-exporter  para /usr/local/bin
 sudo mv node_exporter /usr/local/bin/
 
-# Create node_exporter user
+# Cria o node-exporter user
 sudo useradd node_exporter --no-create-home --shell /bin/false
 
-# Set ownership of node_exporter binary
+# Seta as permissão para o binario do node-exporter
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
-# Create the systemd service file
+# Cria o arquivo de service do systemd
 sudo bash -c 'cat << EOF > /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
@@ -47,7 +47,7 @@ ExecStart=/usr/local/bin/node_exporter
 WantedBy=multi-user.target
 EOF'
 
-# Reload systemd, enable and start node_exporter service
+# Racarregando systemd, enable e start do node-exporter service
 sudo systemctl daemon-reload
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
